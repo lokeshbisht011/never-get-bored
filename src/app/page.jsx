e6201d656b4526f6a863c0080ace11ff55198f32
Lokesh Bisht <lokeshbisht009@gmail.com>
2024-11-30 22:43:06 +0530

    update home screen

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="items-center justify-center flex min-h-screen text-4xl font-[family-name:var(--font-geist-sans)]">
+      NEVER GET BORED
+    </div>
+  );
+}
